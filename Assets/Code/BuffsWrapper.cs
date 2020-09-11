48abadd7007d3f520ef797a6924a6d54f2c779df
frolushka <mpfrolovstud@gmail.com>
2020-09-11 18:54:27 +0300

    update files

--- Assets/Code/BuffsWrapper.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Buffs Wrapper", menuName = "Buffs Wrapper")]
-public class BuffsWrapper : ScriptableObject
-{
-    public List<Buff> buffs;
-}
--- Assets/Code/StatsWrapper.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Stats Wrapper", menuName = "Stats Wrapper")]
-public class StatsWrapper : ScriptableObject
-{
-    public List<Stat> stats;
-}
